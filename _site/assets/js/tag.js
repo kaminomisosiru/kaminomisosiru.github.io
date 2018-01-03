9cead3d7495e98730c1f876923e02639107845f3
Keitaro Hashimoto <kaminomisosiru.414@outlook.com>
2018-01-03 19:47:58 +0900

    Stop tracking

--- _site/assets/js/tag.js
+++
@@ -1,9 +0,0 @@
-var params = location.search.substring(1).split("&")[0].split("=");
-if (params[0] == "tag"){
-    var tag = decodeURI(params[1]);
-    if (tag !== "") {
-        $("#post-list li").hide();
-        $("#post-list li."+tag).show();
-        $("h3.tag-name").text("記事一覧: "+tag);
-    }
-}
